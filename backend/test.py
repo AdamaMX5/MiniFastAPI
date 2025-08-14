4c5f0f23045e6ba0917a24b07f5bb2c7e909d2f5
Kurt <k.ostwald90@gmail.com>
2025-08-14 12:00:44 +0200

    docker compose unterstützung

--- backend/test.py
+++ backend/test.py
@@ -3,3 +3,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+    
+    
+@app.get('/favicon.ico', include_in_schema=False)
+async def default_favicon():
+    return FileResponse("favicon.ico")
